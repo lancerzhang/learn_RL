911213d3a863ae18cdf890c1ad3da50f65966072
lancerz <lancerz@hotmail.com>
2024-06-01 14:56:20 +0800

    save files

--- LunarLander-v2-3.2.py
+++ LunarLander-v2-3.2.py
@@ -1,16 +1,21 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
+import os
+
+models_dir = 'models/PPO'
+
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
 
 env = gym.make('LunarLander-v2', render_mode='human')
-
-model = PPO('MlpPolicy', env, verbose=1)
-model.learn(total_timesteps=100)
-
-episodes = 10
+env.reset()
+model_path = f'{models_dir}/20.zip'
+model = PPO.load(model_path, env=env)
 vec_env = model.get_env()
-obs = vec_env.reset()
 
+episodes = 5
 for ep in range(episodes):
+    obs = vec_env.reset()
     done = False
     while not done:
         action, states = model.predict(obs)
